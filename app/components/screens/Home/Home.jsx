a28ed6e7ea67510cb77432faaf517ac82fd6b1e2
Alexey <metallica2000@list.ru>
2022-02-12 17:09:39 +0500

    create Home screen, HeaderTabs, HeaderButton

--- app/components/screens/Home/Home.jsx
+++ app/components/screens/Home/Home.jsx
@@ -1,12 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
 import HeaderTabs from "../../ui/HeaderTabs";
+import { SafeAreaView } from "react-native-web";
 
 const Home = () => {
   return (
-    <View>
+    <SafeAreaView style={{backgroundColor: '#eee', flex: 1}}>
+   <View style={{backgroundColor: 'white', padding: 15}}>
       <HeaderTabs />
     </View>
+    </SafeAreaView>
   );
 };
 
