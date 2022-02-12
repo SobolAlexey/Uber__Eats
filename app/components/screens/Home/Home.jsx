37857eb78f0fda5ee9a51569e956a0299eff5216
Alexey <metallica2000@list.ru>
2022-02-12 16:27:05 +0500

    start project

---
+++ app/components/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import HeaderTabs from "../../ui/HeaderTabs";
+
+const Home = () => {
+  return (
+    <View>
+      <HeaderTabs />
+    </View>
+  );
+};
+
+export default Home;
