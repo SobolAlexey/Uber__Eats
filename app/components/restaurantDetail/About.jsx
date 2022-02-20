ba8f2beed7f501fc30af63a3b93a65feff259916
Alexey <metallica2000@list.ru>
2022-02-20 23:33:21 +0500

    update About.jsx

--- app/components/restaurantDetail/About.jsx
+++ app/components/restaurantDetail/About.jsx
@@ -1,14 +1,26 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-const image = 'https://media-cdn.tripadvisor.com/media/photo-s/15/9e/05/d1/grilled-prawn.jpg'
-const title = 'Turtle Bay Sea Food Restaurant'
-const description = 'Tai • Comfort Food  • $$ • 🎫 • 4 ⭐ (2913+)'
+const yelpRestaurantInfo = {
+  name: "Turtle Bay Sea Food Restaurant",
+  image: "https://media-cdn.tripadvisor.com/media/photo-s/15/9e/05/d1/grilled-prawn.jpg",
+  reviews: "1500",
+  price: "$$",
+  rating: "4.5",
+  categories: [{title: "Thai"}, {title: "Comfort Food"}]
+}
+
+const {name, image, price, rating, categories, reviews} = yelpRestaurantInfo
+
+const formattedCategories = categories.map(cat => cat.title).join(" • ")
+
+const description =
+ `${formattedCategories} ${price ? " • " + price : ""} • 🎫 • ${rating} ⭐ (${reviews}+)`
 
 const About = () => {
   return (
     <View>
      <RestaurantImage image={image} />
-     <RestaurantTitle title={title} />
+     <RestaurantName name={name} />
      <RestaurantDescription description={description} />
     </View>
   )
@@ -26,14 +38,14 @@ const RestaurantImage = ({image}) => (
   />
 )
 
-const RestaurantTitle = ({title}) => (
+const RestaurantName = ({name}) => (
   <Text style={{
     fontSize: 29,
     marginHorizontal: 15,
     fontWeight: '600',
     marginTop: 10,
   }}>
-  {title}
+  {name}
   </Text>
 )
 
