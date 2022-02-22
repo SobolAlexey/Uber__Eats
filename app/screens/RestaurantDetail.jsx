2ce3635a0d3bc60e28cf6f48e7650a6e10bbced0
Alexey <metallica2000@list.ru>
2022-02-22 17:48:17 +0500

    created ViewCart.jsx

--- app/screens/RestaurantDetail.jsx
+++ app/screens/RestaurantDetail.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import { Divider } from "react-native-elements";
 import About from "../components/restaurantDetail/About";
 import MenuItems from "../components/restaurantDetail/MenuItems";
+import ViewCart from "../components/restaurantDetail/ViewCart"
 
-
-const RestaurantDetail = ({route}) => {
+const RestaurantDetail = ({route, navigation}) => {
   return (
     <View>
       <About route={route} />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
       <MenuItems />
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   );
 };
